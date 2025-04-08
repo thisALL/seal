05ac1cca5d79c1f371675e4193ed10c7e2238ecc
0xbhe <136830425+thisALL@users.noreply.github.com>
2025-04-08 21:51:30 +0700

    Update App.tsx

--- examples/frontend/src/App.tsx
+++ examples/frontend/src/App.tsx
@@ -64,7 +64,7 @@ function App() {
   return (
     <Container>
       <Flex position="sticky" px="4" py="2" justify="between">
-        <h1 className="text-4xl font-bold m-4 mb-8">Seal Example By BHEGHE</h1>
+        <h1 className="text-4xl font-bold m-4 mb-8">Seal Example By Bheghe</h1>
         {/* <p>TODO: add seal logo</p> */}
         <Box>
           <ConnectButton />
